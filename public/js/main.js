cbfe7a2cb8a9cbda6de0cfea281e21d71becd436
Zaven Muradyan <megalivoithos@gmail.com>
2013-09-06 23:28:14 -0700

    Remove unused s.format() function

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,3 @@
-if (!String.prototype.format) {
-    String.prototype.format = function() {
-        var args = arguments;
-        return this.replace(/{(\d+)}/g, function(match, number) { 
-            return typeof args[number] != 'undefined'
-                ? args[number]
-                : match;
-        });
-    };
-}
-
 // Initiate parsley validation
 $(document).ready(function() {
     $('.form-parsley').each(function(i, elem) {
