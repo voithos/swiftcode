de1ce92740b756f137d6af54bb4d9e994cf3bdb1
Zaven Muradyan <megalivoithos@gmail.com>
2013-09-20 17:50:36 -0700

    Add info logging to runserver.js and workaround bug in forever-monitor
    A bug existed in forever-monitor which caused the
    ignorePatterns to be ignored (haha). It was replaced
    with a temporary fork which fixes the problem.

--- runserver.js
+++ runserver.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 var forever = require('forever-monitor');
 
-var server = new (forever.Monitor)('server.js', {
+// TODO: Use official forever repo as dependency once
+// ignorePattern bug has been fixed
+var server = new forever.Monitor('server.js', {
     options: process.argv.slice(2),
     watch: true,
-    watchIgnorePatterns: ['public', 'views'],
+    watchIgnorePatterns: ['public/**', 'views/**'],
     watchDirectory: '.'
 });
 
+server.on('watch:restart', function(info) {
+    console.error('    restarting script because ' + info.file + ' changed');
+});
+server.on('restart', function() {
+    console.error('    (time ' + server.times + ')');
+});
+server.on('exit:code', function(code) {
+    if (code) {
+        console.error('    script exited with code ' + code);
+    }
+});
 server.on('exit', function() {
     console.log('server.js exiting');
 });
